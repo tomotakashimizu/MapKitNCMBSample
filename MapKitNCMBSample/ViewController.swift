93569147e8954585ba370e6c8d8d5a2de48f62a9
tomotakashimizu <tomotaka.developer@gmail.com>
2020-10-06 17:31:19 +0900

    Initial Commit

---
+++ MapKitNCMBSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitNCMBSample
+//
+//  Created by 清水智貴 on 2020/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
