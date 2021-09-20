dd426dcebaa6de9ff6903e318b99afb49b82b435
tomotakashimizu <tomotaka.developer@gmail.com>
2021-09-20 19:04:38 +0900

    [update] annotation がユーザの現在地の場合と投稿の場合で pin を変更

--- MapKitNCMBSample/ViewControllers/Main/AllPostedPlacePointViewController.swift
+++ MapKitNCMBSample/ViewControllers/Main/AllPostedPlacePointViewController.swift
@@ -16,6 +16,7 @@ class AllPostedPlacePointViewController: UIViewController {
     var posts = [String: [Post]]()
     var annotationList = [MKPointAnnotation]()
     var selectedCoordinate = CLLocationCoordinate2D()
+    var locationManager: CLLocationManager!
     
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var searchBar: UISearchBar!
@@ -99,6 +100,13 @@ class AllPostedPlacePointViewController: UIViewController {
             }
         })
     }
+    
+    @IBAction func toCurrentLocation() {
+        // 現在地に移動
+        mapView.setCenter(mapView.userLocation.coordinate, animated: true)
+        print("latitude: \(mapView.userLocation.coordinate.latitude)")
+        print("longitude: \(mapView.userLocation.coordinate.longitude)")
+    }
 }
 
 
@@ -107,10 +115,19 @@ extension AllPostedPlacePointViewController: MKMapViewDelegate {
     
     func configureMapView() {
         mapView.delegate = self
+        
+        // 現在位置表示の有効化
+        mapView.showsUserLocation = true
+        // 現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される）
+        mapView.userTrackingMode = .followWithHeading
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
+        // annotation がユーザの現在地の場合
+        if (annotation is MKUserLocation) {
+            // デフォルト
+            return nil
+        }
         let pinView = MKPinAnnotationView()
         pinView.animatesDrop = false
         pinView.isDraggable = false
@@ -122,9 +139,8 @@ extension AllPostedPlacePointViewController: MKMapViewDelegate {
         button.setTitle("この地点の投稿を見る", for: .normal)
         button.backgroundColor = UIColor.blue
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 12)
-        pinView.rightCalloutAccessoryView = button
         
-        //mapView.addAnnotations(annotationList)
+        pinView.rightCalloutAccessoryView = button
         
         return pinView
     }
@@ -153,6 +169,43 @@ extension AllPostedPlacePointViewController: MKMapViewDelegate {
             mapView.mapType = .standard
         }
     }
+}
+
+
+// MARK:- CLLocationManagerDelegate に関する処理
+extension AllPostedPlacePointViewController: CLLocationManagerDelegate {
+    
+    // locationManagerの設定
+    func setupLocationManager() {
+        // locationManagerオブジェクトの初期化
+        locationManager = CLLocationManager()
+        
+        // locationManagerオブジェクトが初期化に成功している場合のみ許可をリクエスト
+        guard let locationManager = locationManager else { return }
+        
+        // ユーザに対して、位置情報を取得する許可をリクエスト
+        locationManager.requestWhenInUseAuthorization()
+        
+        // ユーザから「アプリ使用中の位置情報取得」の許可が得られた場合のみ、マネージャの設定を行う
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            
+            // ViewControllerクラスが管理マネージャのデリゲート先になるように設定
+            locationManager.delegate = self
+            // メートル単位で設定
+            locationManager.distanceFilter = 10
+            // 位置情報の取得を開始
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    // 現在の位置情報を取得・更新するたびに呼ばれるデリゲートメソッド
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 現在位置が更新される度に地図の中心位置を変更する（アニメーション）
+        // ユーザが向いている方向も表示
+        mapView.userTrackingMode = .followWithHeading
+    }
+    
 }
 
 
